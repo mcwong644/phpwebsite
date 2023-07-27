5357e20ab6f99ce2176476aa20711fe18733bfd9
mcwong644 <mcwong644@gmail.com>
2023-07-27 22:04:23 +0800

    july27b

--- tm_script.js
+++ tm_script.js
@@ -1,10 +1,19 @@
 // https://youtu.be/BI1o2H9z9fo
 //Modern JavaScript From The Beginning | First 12 Hours
-//
+// stop 00:36:23
+
 const x=100
 
 console.log(x);
 console.error('Error!!');
 console.warn('warning');
-console.table("testing",23,true);
+console.table({name:"testing",age:2300, email:"soso@gmail.com",married:true});
 
+console.group('simple');
+console.log('inside simple');
+console.log('end of simple');
+
+console.groupEnd
+const styles = 'padding: 10px; backgeound-color: yellow;color: green';
+// use %c to use styles
+console.log('%chellooo World!!!',styles);
