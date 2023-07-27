d4441e21fe5e7732f7b2713ae3d1b800489cf262
mcwong644 <mcwong644@gmail.com>
2023-07-27 18:17:54 +0800

    july27

---
+++ tm_script.js
@@ -0,0 +1,10 @@
+// https://youtu.be/BI1o2H9z9fo
+//Modern JavaScript From The Beginning | First 12 Hours
+//
+const x=100
+
+console.log(x);
+console.error('Error!!');
+console.warn('warning');
+console.table("testing",23,true);
+
