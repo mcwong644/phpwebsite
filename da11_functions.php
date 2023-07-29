e4d5c43216753056f7fcf6aca98d49430ebbe77f
mcwong644 <mcwong644@gmail.com>
2023-07-29 21:40:52 +0800

    july29

---
+++ da11_functions.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+<?php
+
+$name="Dary";
+echo "my name is {$name}";
+echo "<br>";
+// need double quotes for variable substitution ""
+echo 'my name is {$name}';
+?>    
+</body>
+</html>
+
+<?php
+// https://youtu.be/cGwSm8xDSwI
+// PHP & MySQL Tutorial | Learn PHP From Scratch | Full Tutorial
+// start 02:21:17 intro to functions 
+// stop 52.38
+
+
+?>
