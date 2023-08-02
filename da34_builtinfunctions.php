44cb03a1430d88251fb7391703491119bee85718
mcwong644 <mcwong644@gmail.com>
2023-08-02 22:10:40 +0800

    aug2

---
+++ da34_builtinfunctions.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // https://youtu.be/cGwSm8xDSwI
+// PHP & MySQL Tutorial | Learn PHP From Scratch | Full Tutorial
+// start 02:26:17 create 1st functions
+// stop 03:20:00
+
+    //builtin functions
+
+    //math
+    echo ceil(1.50) . "<br>";
+    echo floor(1.6)."<br>";
+    echo round(0.54335. 2)."<br>"    ;
+    echo exp(4)."<br>"; //exponent
+    //string
+
+    //date
+    //array
+    //random number
+
+    ?>
+    
+</body>
+</html>
